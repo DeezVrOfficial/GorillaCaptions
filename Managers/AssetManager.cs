b8f30736815f80f354abe9f895f06f9bdf23258a
DeezVr <normajperez1030@gmail.com>
2026-05-31 16:09:54 -0400

    Add files via upload

---
+++ Managers/AssetManager.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace GorillaCaptions.Managers
+{
+    public class AssetManager
+    {
+        public static AssetBundle assetBundle;
+
+        private static void LoadAssetBundle()
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("GorillaCaptions.Resources.caption");
+
+            if (stream == null)
+                assetBundle = AssetBundle.LoadFromStream(stream);
+            else
+                Debug.LogError("[GorillaCaptions] Failed to load AssetBundle");
+        }
+
+        public static T LoadObject<T>(string assetName) where T : Object
+        {
+            T gameObject = null;
+
+            if (assetBundle == null)
+                LoadAssetBundle();
+
+            gameObject = Object.Instantiate(assetBundle.LoadAsset<T>(assetName));
+            return gameObject;
+        }
+
+        public static T LoadAsset<T>(string assetName) where T : Object
+        {
+            T gameObject = null;
+
+            if (assetBundle == null)
+                LoadAssetBundle();
+
+            gameObject = assetBundle.LoadAsset(assetName) as T;
+            return gameObject;
+        }
+    }
+}
