dc01d3e498c756dc6b51ae9d6f0995b97e9395e2
DeezVr <normajperez1030@gmail.com>
2026-05-31 16:05:03 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace GorillaCaptions
+{
+    [BepInPlugin(PluginInfo.Guid, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Plugin instance;
+
+        void Awake()
+        {
+            instance = this;
+            Debug.Log("<GorillaCaptions> Created by Deez & Goldentrophy <3");
+            Debug.Log("Rip GoldenTrophy (iiDk) Dec 2019 - Feb 23, 2026 <3");
+        }
+
+        void Start()
+        {
+            HarmonyPatches.ApplyHarmonyPatches();
+
+            GameObject ClassHolder = new GameObject("GorillaCaptions");
+            ClassHolder.AddComponent<Managers.SynthesizerManager>();
+            ClassHolder.AddComponent<Managers.BubbleManager>();
+            DontDestroyOnLoad(ClassHolder);
+
+            Debug.Log("<GorillaCaptions> Fully initialized!");
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace GorillaCaptions
+{
+    internal class PluginInfo
+    {
+        public const string Guid = "com.goldentrophy.deez.gorillacaptions";
+        public const string Name = "Deez's Gorilla Captions";
+        public const string Version = "1.0.2";
+    }
+}
